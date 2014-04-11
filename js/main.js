f0f4e9f17bf3cc26d19e6759f07eaf7137156ed6
Jason Malia <jmalia3@gatech.edu>
2014-04-11 00:53:41 -0400

    partial zoom

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@
 <!-- Tooltip example from http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html -->
 
 var margin = {top: 40, right: 20, bottom: 30, left: 40},
-    width = 650 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    w = width = 650.0 - margin.left - margin.right,
+    h = height = 500.0 - margin.top - margin.bottom,
+	x = d3.scale.linear().range([0, width]),
+    y = d3.scale.linear().range([0, height]);
+	
+var x_root, x_node;
 
 /* 
  * value accessor - returns the value to encode for a given data object.
@@ -30,25 +34,34 @@ var yValue = function(d) { return d[season+"Win"];}, // data -> value
 // setup fill color
 var cValue = function(d) { return d.Division;},
     color = d3.scale.ordinal().range(["#fbb4ae","#b3cde3","#ccebc5","#decbe4","#fed9a6","#ffffcc"]);
-	//colorSP = d3.scale.ordinal().range(["#fbb4ae","#b3cde3","#ccebc5","#decbe4","#fed9a6","#ffffcc"]);
 
 // add the sp graph canvas to its appropriate div
 var svg = d3.select("#sp_vis").append("svg")
 	 .style("float", "left")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+     .attr("width", width + margin.left + margin.right)
+     .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
 
 // add tm graph canvas to its appropriate div
-var div2 = d3.select("#tm_vis").append("div")
+/*var div2 = d3.select("#tm_vis").append("div")
     .style("position", "relative")
 	.style("float", "right")
     .style("width", (width + margin.left + margin.right) + "px")
     .style("height", (height + margin.top + margin.bottom) + "px")
     .style("left", margin.left + "px")
     .style("top", margin.top + "px");
+	*/
+var div2 = d3.select("#tm_vis").append("div")
+    .attr("class", "chart")
+    .style("width", (width + margin.left + margin.right) + "px")
+    .style("height", (height + margin.top + margin.bottom) + "px")
+  .append("svg:svg")
+    .style("width", (width) + "px")
+    .style("height", (height) + "px")
+  .append("svg:g")
+    .attr("transform", "translate(.5,.5)");
 	
 var treemap = d3.layout.treemap()
     .size([width, height])
@@ -68,33 +81,227 @@ d3.csv("../data/nba.csv", function(error, data) {
    // getting data for scatterplot
 	data.forEach(function(d) {
 	  for (i = 1; i < 13; ++i) {
-	  salary = sprintf("%02d-%02d Salary", i, i+1);
-	  win = sprintf("%02d-%02d Win", i, i+1);
-	  loss = sprintf("%02d-%02d Loss", i, i+1);
-	  d[salary] = (+d[salary])/1000000;
-	  d[win] = +d[win];
-	  d[loss] = +d[loss];	  
+		  salary = sprintf("%02d-%02d Salary", i, i+1);
+		  win = sprintf("%02d-%02d Win", i, i+1);
+		  loss = sprintf("%02d-%02d Loss", i, i+1);
+		  d[salary] = (+d[salary])/1000000;
+		  d[win] = +d[win];
+		  d[loss] = +d[loss];	  
 	  }
 	});
 
 
 	<!------------------------TREEMAP---------------------------->
+
+	// Converting the data
+	var preppedData = genJSON(data, ['Conference', 'Division']);
+	x_node = x_root = preppedData;
+
+	var nodes = treemap.nodes(x_root)
+      .filter(function(d) { return !d.children; });
+
+/*	  
+	var node = div2.selectAll(".node")
+		.data(nodes)
+	.enter().append("div")
+		.attr("class", "node")
+		.call(position)
+		.style("background", function(d) { return d.children ?  null: color(d['Division']) ; })
+		.text(function(d) { return d.children ? null : d["Team"]; })
+		.attr("id", function(d) { return d.children ? null : "tm"+d["Team"]; } )
+		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      	.on("click", function(d) { return zoom(x_node == d.parent ? x_root : d.parent); })
+		.on("mouseover", function(d) { details_on_demand(d); } )
+		.on("mouseout", function(d) { details_off(d); } );
+	*/
+
   
-	_.each(data, function(element, index, list){
-		element[season+"Salary"] = +element[season+"Salary"];
-	});
 
-	//*************************************************
-	// THE FUNCTION
-	//*************************************************
+  var cell = div2.selectAll("g")
+      .data(nodes)
+    .enter().append("svg:g")
+      .attr("class", "cell")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .on("click", function(d) { return zoom(x_node == d.parent ? x_root : d.parent); });
+
+  cell.append("svg:rect")
+      .attr("width", function(d) { return d.dx - 1; })
+      .attr("height", function(d) { return d.dy - 1; })
+      .style("fill", function(d) { return color(d.Division); });
+
+ cell.append("svg:text")
+      .attr("x", function(d) { return 5; })
+      .attr("y", function(d) { return 19; })
+     .attr("dy", ".35em")
+      .text(function(d) { return d["Team"]; });
+     // .style("opacity", function(d) { d.w = this.getComputedTextLength(); return d.dx > d.w ? 1 : 0; });
+
+		
+/*	d3.selectAll("input").on("change", function change() {
+		var value = this.value == "count"
+			? function() { return 1; }
+			: function(d) { return d[season+"Salary"]; };
+
+		node
+			.data(treemap.value(value).nodes)
+		 .transition()
+			.duration(1500)
+			.call(position);
+			
+	}); 
+*/
+	
+	 d3.select(window).on("click", function() { zoom(x_root); });
+
+	  d3.select("select").on("change", function(d) {
+		treemap.value(this.value == d[season+"Salary"] ? size(d) : count).nodes(x_root);
+		zoom(x_node);
+  });
+
+  
+	function position() {
+		this.style("left", function(d) { return d.x + "px"; })
+		  .style("top", function(d) { return d.y + "px"; })
+		  .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+		  .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+	}
+  
+	/* ---------------------------SCATTERPLOT------------------------------ */
+	xScale.domain([30, 130]);
+	yScale.domain([0, 70]);
+	
+	// x-axis
+	svg.append("g")
+	  .attr("class", "x axis")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(xAxis)
+	.append("text")
+	  .attr("class", "label")
+	  .attr("x", width)
+	  .attr("y", -6)
+	  .style("text-anchor", "end")
+	  .text("Team Salary ($ Million)");
+	
+	// y-axis
+	svg.append("g")
+	  .attr("class", "y axis")
+	  .call(yAxis)
+	.append("text")
+	  .attr("class", "label")
+	  .attr("transform", "rotate(-90)")
+	  .attr("y", 6)
+	  .attr("dy", ".71em")
+	  .style("text-anchor", "end")
+	  .text("Wins");
+	
+	// draw dots
+	svg.selectAll(".dot")
+	  .data(data)
+	.enter().append("circle")
+	  .attr("id", function(d) {return "sp"+d["Team"];})
+	  .attr("class", "dot")
+	  .attr("r", 3.5)
+	  .attr("cx", xMap)
+	  .attr("cy", yMap)
+	  .style("fill", function(d) { return color(cValue(d));}) 
+	  .on("mouseover", function(d) { details_on_demand(d); })
+	  .on("mouseout", function(d) { details_off(d); });
+	  
+	// draw legend
+	var legend = svg.selectAll(".legend")
+	  .data(color.domain())
+	.enter().append("g")
+	  .attr("class", "legend")
+	  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+	
+	// draw legend colored rectangles
+	legend.append("rect")
+	  .attr("x", width - 18)
+	  .attr("width", 18)
+	  .attr("height", 18)
+	  .style("fill", color);
+	
+	// draw legend text
+	legend.append("text")
+	  .attr("x", width - 24)
+	  .attr("y", 9)
+	  .attr("dy", ".35em")
+	  .style("text-anchor", "end")
+	  .text(function(d) { return d;})
+
+	function size(d) {
+	  return d[season+"Salary"];
+	}
+	
+	function count(d) {
+	  return 1;
+	}
+	  
+	function zoom(d) {
+	  var kx = width / d.dx, ky = height / d.dy;
+	  x.domain([d.x, d.x + d.dx]);
+	  y.domain([d.y, d.y + d.dy]);
+	
+	  var t = div2.selectAll("g.cell").transition()
+		  .duration(d3.event.altKey ? 7500 : 750)
+		  .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+	
+	  t.select("rect")
+		  .attr("width", function(d) { return kx * d.dx - 1; })
+		  .attr("height", function(d) { return ky * d.dy - 1; })
+	
+	  t.select("text")
+		  .attr("x", function(d) { return 5 })
+		  .attr("y", function(d) { return 10; })
+	//	  .style("opacity", function(d) { return kx * d.dx > d.w ? 1 : 0; });
+	
+	  x_node = d;
+	  d3.event.stopPropagation();
+	}
+	
+	function details_on_demand(d) {
+	  
+	document.getElementById("tm"+d.Team).style.border = "1px solid black";
+	document.getElementById("tm"+d.Team).style.zIndex = "40000";
+	document.getElementById("sp"+d.Team).setAttribute("r", 7);
+	document.getElementById("sp"+d.Team).style.zIndex = "100000";
+	
+	tooltip.transition()
+	   .duration(200)
+	   .style("opacity", 1)
+	   .style("background", "#FFFFFF")
+	   .style("max-width", "140px")
+	   .style("height", "auto");
+	   
+	tooltip.html("<b>" + d["Team"] + "</b><br/>\t  Salary: <b>" + curr_fmt(xValue(d)*1000000)
+	+ "</b><br/>\t  Wins: <b>" + yValue(d) + "</b>; Losses: <b>" + d[season+"Loss"] + "</b>")
+	   .style("left",  d["Team"] ? (document.getElementById("sp"+d["Team"]).getBoundingClientRect().left + 16) + "px": null)
+	   .style("top", d["Team"] ? (document.getElementById("sp"+d["Team"]).getBoundingClientRect().top - 18) + "px": null)
+	   .style("padding", "5px")
+	   .style("padding-left", "10px");
+	}
+	
+	function details_off(d) {
+	  
+	  document.getElementById("sp"+d.Team).setAttribute("r", 3.5);
+	  document.getElementById("sp"+d.Team).style.zIndex = "30000";
+	  document.getElementById("tm"+d["Team"]).style.border = "1px solid white";
+	  document.getElementById("tm"+d["Team"]).style.zIndex = "20000";
+	  
+	  tooltip.transition()
+		   .duration(500)
+		   .style("opacity", 0);
+	}
+  
+	// Convert .csv data to json format for tree map
 	function genJSON(csvData, groups) {
-
+	
 	  var genGroups = function(data) {
 		return _.map(data, function(element, index) {
 		  return { name : index, children : element };
 		});
 	  };
-
+	
 	  var nest = function(node, curIndex) {
 		if (curIndex === 0) {
 		  node.children = genGroups(_.groupBy(csvData, groups[0]));
@@ -116,157 +323,5 @@ d3.csv("../data/nba.csv", function(error, data) {
 	  };
 	  return nest({}, 0);
 	}
-	//*************************************************
-	// CALL THE FUNCTION
-	//*************************************************
-	var preppedData = genJSON(data, ['Conference', 'Division'])
-
-	//*************************************************
-	// LOAD THE PREPPED DATA IN D3
-	//*************************************************
-	
-	var node = div2.datum(preppedData).selectAll(".node")
-		.data(treemap.nodes)
-	.enter().append("div")
-		.attr("class", "node")
-		.call(position)
-		.style("background", function(d) { return d.children ?  null: color(d['Division']) ; })
-		.text(function(d) { return d.children ? null : d["Team"]; })
-		.attr("id", function(d) { return d.children ? null : "tm"+d["Team"]; } )
-		.on("mouseover", function(d) {
-			document.getElementById("tm"+d.Team).style.border = "1px solid black";
-		    document.getElementById("tm"+d.Team).style.zIndex = "40000";
-			document.getElementById("sp"+d.Team).setAttribute("r", 7);
-			document.getElementById("sp"+d.Team).style.zIndex = "100000";
-			tooltip.transition()
-               .duration(200)
-               .style("opacity", 1)
-			   .style("background", "#FFFFFF")
-			   .style("max-width", "140px")
-			   .style("height", "auto");
-          tooltip.html("<b>" + d["Team"] + "</b><br/>\t  Salary: <b>" + curr_fmt(xValue(d)*1000000)
-	        + "</b><br/>\t  Wins: <b>" + yValue(d) + "</b>; Losses: <b>" + d[season+"Loss"] + "</b>")
-               .style("left",  d["Team"] ? (document.getElementById("sp"+d["Team"]).getBoundingClientRect().left + 16) + "px": 
-			   (d3.event.pageX + 5) + "px")
-               .style("top", d["Team"] ? (document.getElementById("sp"+d["Team"]).getBoundingClientRect().top - 18) + "px": (d3.event.pageY - 28) + "px")
-			   .style("padding", "5px")
-			   .style("padding-left", "10px")})
-      .on("mouseout", function(d) {
-		  document.getElementById("sp"+d.Team).setAttribute("r", 3.5);
-		  document.getElementById("sp"+d.Team).style.zIndex = "30000";
-		  document.getElementById("tm"+d["Team"]).style.border = "1px solid white";
-		  document.getElementById("tm"+d["Team"]).style.zIndex = "20000";
-          tooltip.transition()
-               .duration(500)
-               .style("opacity", 0);
-      });
-		
-	d3.selectAll("input").on("change", function change() {
-		var value = this.value == "count"
-			? function() { return 1; }
-			: function(d) { return d[season+"Salary"]; };
-
-		node
-			.data(treemap.value(value).nodes)
-		 .transition()
-			.duration(1500)
-			.call(position);
-			
-	}); 
-	
-  
-  	function position() {
-  	this.style("left", function(d) { return d.x + "px"; })
-      .style("top", function(d) { return d.y + "px"; })
-      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
-      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
-	}
-  
-  /* ---------------------------SCATTERPLOT------------------------------ */
-  xScale.domain([30, 130]);
-  yScale.domain([0, 70]);
-
-  // x-axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Team Salary ($ Million)");
-
-  // y-axis
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Wins");
-
-  // draw dots
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-	  .attr("id", function(d) {return "sp"+d["Team"];})
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));}) 
-      .on("mouseover", function(d) {
-		  document.getElementById("sp"+d.Team).setAttribute("r", 7);
-		  document.getElementById("tm"+d["Team"]).style.border = "1px solid black";
-		  document.getElementById("tm"+d["Team"]).style.zIndex = "50000";
-          tooltip.transition()
-               .duration(200)
-               .style("opacity", 1)
-			   .style("background", "#FFFFFF")
-			   .style("max-width", "140px")
-			   .style("height", "auto");
-          tooltip.html("<b>" + d["Team"] + "</b><br/>\t  Salary: <b>" + curr_fmt(xValue(d)*1000000)
-	        + "</b><br/>\t  Wins: <b>" + yValue(d) + "</b>; Losses: <b>" + d[season+"Loss"] + "</b>")
-               .style("left", (d3.event.pageX + 10) + "px")
-               .style("top", (d3.event.pageY - 25) + "px")
-			   .style("padding", "5px")
-			   .style("padding-left", "10px");
-      })
-      .on("mouseout", function(d) {
-		 document.getElementById("sp"+d.Team).setAttribute("r", 3.5);
-		 document.getElementById("tm"+d["Team"]).style.border = "1px solid white";
-		 document.getElementById("tm"+d["Team"]).style.zIndex = "40000";
-         tooltip.transition()
-               .duration(200)
-               .style("opacity", 0);
-			   
-      });
-
-  // draw legend
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  // draw legend text
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d;})
-	
+  	
 });
